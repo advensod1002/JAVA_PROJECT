feff5afc65ad41e5d7daaca514fbd5e4001e88fe
Kim Beom Jin <54887131+advensod1002@users.noreply.github.com>
2021-09-01 17:55:33 +0900

    Add files via upload

---
+++ JAVA/Day8/com/day8b/User3Test.java
@@ -0,0 +1,14 @@
+package com.jcp.day8b;
+
+import com.jcp.day8a.User3;
+
+public class User3Test {
+
+	public static void main(String[] args) {
+		
+		User3 momo = new User3();
+		
+		momo.name = "모모";
+	}
+
+}
